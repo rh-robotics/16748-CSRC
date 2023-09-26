58467d99869218d7d1b5222524e2c17d4b8b0aa0
isaccbarker <milobanks@rowlandhall.org>
2023-09-26 16:10:05 -0600

    [base] bring up and sync

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Stores and Declares all hardware devices &amp; related methods
+ */
+public class HWC {
+    // Declare empty variables for robot hardware
+    public DcMotorEx leftFront, rightFront, leftRear, rightRear;
+
+    // Other Variables
+    Telemetry telemetry;
+    ElapsedTime time = new ElapsedTime();
+
+    /**
+     * Constructor for HWC, declares all hardware components
+     *
+     * @param hardwareMap HardwareMap - Used to retrieve hardware devices
+     * @param telemetry   Telemetry - Used to add telemetry to driver hub
+     */
+    public HWC(@NonNull HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+
+        // Declare motors
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+
+        // Set the direction of motors
+        // TODO: UPDATE VALUES WITH NEW BOT
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.FORWARD);
+        leftRear.setDirection(DcMotorEx.Direction.FORWARD);
+        rightRear.setDirection(DcMotorEx.Direction.FORWARD);
+
+        // Set motors to break when power = 0
+        // TODO: REMOVE IF THIS BEHAVIOUR IS NOT DESIRED ON NEW BOT
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Run motors using encoder, so that we can move accurately.
+        leftFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        leftRear.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+    }
+    // TODO: ADD ANY HARDWARE RELATED FUNCTIONS BELOW
+}
