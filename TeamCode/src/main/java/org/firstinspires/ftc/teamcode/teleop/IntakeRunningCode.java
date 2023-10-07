0df28b3bd24d02b5c49fbdadd27a8aa5528a6d55
Picklze1 <georgejensen@rowlandhall.org>
2023-10-07 01:55:46 -0600

    Comments fixed 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IntakeRunningCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IntakeRunningCode.java
@@ -4,15 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 // Defines Class IntakeRunningCode
-
 public class IntakeRunningCode extends OpMode{
 
     // Defines a Moter called intakeMotor
-
     DcMotor intakeMotor;
 
     // Adds intakeMotor to the HardwareMap
-
     public void init() {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
     }
@@ -21,7 +18,6 @@ public class IntakeRunningCode extends OpMode{
 
         /* While the button a is pressed the motor will run a 0.5 and
         will add telemetry that the intake motor is running */
-
         while (gamepad1.a) {
             intakeMotor.setPower(0.5);
             telemetry.addData("Intake Motor", "Running");
