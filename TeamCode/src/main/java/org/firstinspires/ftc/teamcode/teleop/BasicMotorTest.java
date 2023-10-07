e97e494e865f05236705c9006899d64033ef72b5
RZW-13 <rayneweinstein@gmail.com>
2023-10-07 02:59:20 -0600

    fixed comment issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicMotorTest.java
@@ -7,30 +7,27 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.subsystems.HWC;
 
+// "BasicMotorTest" is the name of OpMode in Driver Station.
 /**
  * TeleOp OpMode for testing motors.
  */
-// "BasicMotorTest" is the name of OpMode in Driver Station
 @TeleOp(name = "BasicMotorTest", group = "Iterative OpMode")
-
 public class BasicMotorTest extends OpMode {
     private final ElapsedTime time = new ElapsedTime();
-    HWC robot; // Declares roboBt as an instance of a hardware class.
+    // Declares robot as an instance of a hardware class.
+    private HWC robot;
 
     // Runs once when "INIT" button is pressed.
     @Override
     public void init() {
         telemetry.addData("Status", "Initializing");
         // Telemetry.addData stores data in a hashmap-like data set.
-        robot = new HWC(hardwareMap, telemetry); // assigns robot value
+        robot = new HWC(hardwareMap, telemetry); // Assigns robot value.
         telemetry.addData("Status", "Initialized"); // Updates "Status" key's value.
     }
 
-    // Runs repetitively between "INIT" and "START" button presses,
-    // FRC SDK default version does nothing.
-    @Override
-    public void init_loop() {
-    }
+    /* Method init_loop() runs repetitively between "INIT" and "START" button presses,
+     * FTC SDK default version does nothing. */
 
     // Runs once after "START" button pressed.
     @Override
@@ -38,16 +35,16 @@ public class BasicMotorTest extends OpMode {
         time.reset();
     }
 
-    // Runs forever after "START" button pressed
-    // (unless given an exception or "STOP" button pressed).
+    /* Runs forever after "START" button pressed
+     * (unless given an exception or "STOP" button pressed). */
     @Override
     public void loop() {
-        if (gamepad1.a) { // press button 'A'
+        if (gamepad1.a) {
             robot.leftFront.setPower(1.0); // set motor power
             robot.leftRear.setPower(1.0);
             robot.rightFront.setPower(1.0);
             robot.rightRear.setPower(1.0);
-        } else if (gamepad1.b) { // press button 'B'
+        } else if (gamepad1.b) {
             robot.leftFront.setPower(0.5);
             robot.leftRear.setPower(0.5);
             robot.rightFront.setPower(0.5);
