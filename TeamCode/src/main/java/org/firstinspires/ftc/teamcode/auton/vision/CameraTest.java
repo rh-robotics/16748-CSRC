8f27babe4be9bfcd78de91f42c8b4f3a5856107e
milobanks <milobanks@rowlandhall.org>
2023-10-08 22:31:50 -0600

    [format] remove newline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/vision/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/vision/CameraTest.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-
 import org.firstinspires.ftc.teamcode.subsystems.vision.SleeveDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
