c4097b6fad4d278f63cd2488b8697a1697d8d2dc
Picklze1 <georgejensen@rowlandhall.org>
2023-10-08 22:24:44 -0600

    Deleting unnessesary imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/vision/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/vision/CameraTest.java
@@ -4,8 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-//import org.firstinspires.ftc.teamcode.subsystems.vision.SleeveDetection;
-//import org.firstinspires.ftc.teamcode.common.powerplay.SleeveDetection;
+
 import org.firstinspires.ftc.teamcode.subsystems.vision.SleeveDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
