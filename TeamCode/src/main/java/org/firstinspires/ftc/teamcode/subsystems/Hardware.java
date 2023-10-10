7f767b55cfe3e6c50ca6a30599faeb0c11391308
RZW-13 <rayneweinstein@gmail.com>
2023-10-09 18:04:08 -0600

    added deviceType-specific init code and refactored

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hardware.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 import androidx.annotation.NonNull;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -8,30 +11,103 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/** eg. initialization code
+ *
+ * // class declaration
+ * public Hardware robot;
+ *
+ * // in init
+ * robot = new Hardware();
+ * robot.introduce(new HardwareElement(hardwareMap, DcMotorEx.class, "leftFront"));
+ * robot.init();
+ *
+ * // in loop
+ * robot.HardwareElements.get("leftFront").device.setPower(0.5);
+ */
+
 public class Hardware {
 
-    private final Telemetry telemetry;
-    private final HardwareMap hardwareMap;
-    /** ArrayList to hold all hardware elements. */
-    private ArrayList<HardwareElement> hardwareElements = new ArrayList<HardwareElement>();
+    /** HashMap to hold all hardware elements in name/entry:hardwareElement format. */
+    private HashMap<String, HardwareElement> hardwareElements;
 
-    public Hardware(@NonNull Telemetry telemetry, @NonNull HardwareMap hardwareMap) {
-        this.telemetry = telemetry;
-        this.hardwareMap = hardwareMap;
+    /** Object that contains hardwareElements HashMap. */
+    public Hardware() {
+        this.hardwareElements = new HashMap<>();
     }
 
-    /** Adds hardwareElement to hardwareElements arrayList */
+    /** Adds hardwareElement to hardwareElements arrayList in entry:hardwareElement format.*/
     public void introduce(HardwareElement hardwareElement){
-        this.hardwareElements.add(hardwareElement);
+        this.hardwareElements.put(hardwareElement.entry, hardwareElement);
+    }
+
+    public void init(){
+        for (int i = 0; i < this.hardwareElements.size(); i++) {
+            HardwareElement currentElement = this.hardwareElements.get(i);
+            java.lang.Class<HardwareDevice> hardwareType = currentElement.type;
+            HashMap<String,String> initsHashMap = currentElement.initializers;
+
+            /** Initializers should be added as needed to the conditional with their corresponding class. */
+            if (hardwareType.getName().equals("DcMotor")) {
+                /* Casting a separate deviceDcMotor object as a DcMotor type allows us
+                 * run type-specific methods. (eg. deviceDcMotor.setDirection()) */
+                DcMotor deviceDcMotor = (DcMotor) currentElement.device;
+
+                if (initsHashMap.get("setDirection").equals("FORWARD")) {
+                    deviceDcMotor.setDirection(DcMotorEx.Direction.FORWARD);
+                } else if (initsHashMap.get("setDirection").equals("REVERSE")) {
+                    deviceDcMotor.setDirection(DcMotor.Direction.REVERSE);
+                } else {
+                    InitValueRuntimeException("setDirection", initsHashMap.get("setDirection"));
+                }
+
+                if (initsHashMap.get("setZeroPowerBehavior").equals("BRAKE")) {
+                    deviceDcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                } else if (initsHashMap.get("setZeroPowerBehavior").equals("FLOAT")) {
+                    deviceDcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                } else {
+                    InitValueRuntimeException("setZeroPowerBehavior",
+                            initsHashMap.get("setZeroPowerBehavior"));
+                }
+
+                if (initsHashMap.get("setMode").equals("RUN_USING_ENCODER")) {
+                    deviceDcMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                } else {
+                    InitValueRuntimeException("setMode", initsHashMap.get("setMode"));
+                }
+            } else if (hardwareType.getName().equals("DcMotor")) {
+                DcMotorEx deviceDcMotorEx = (DcMotorEx) currentElement.device;
+
+                if (initsHashMap.get("setDirection").equals("FORWARD")) {
+                    deviceDcMotorEx.setDirection(DcMotorEx.Direction.FORWARD);
+                } else if (initsHashMap.get("setDirection").equals("REVERSE")) {
+                    deviceDcMotorEx.setDirection(DcMotorEx.Direction.REVERSE);
+                } else {
+                    InitValueRuntimeException("setDirection", initsHashMap.get("setDirection"));
+                }
+
+                if (initsHashMap.get("setZeroPowerBehavior").equals("BRAKE")) {
+                    deviceDcMotorEx.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+                } else if (initsHashMap.get("setZeroPowerBehavior").equals("FLOAT")) {
+                    deviceDcMotorEx.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+                } else {
+                    InitValueRuntimeException("setZeroPowerBehavior",
+                            initsHashMap.get("setZeroPowerBehavior"));
+                }
+
+                if (initsHashMap.get("setMode").equals("RUN_USING_ENCODER")) {
+                    deviceDcMotorEx.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+                } else {
+                    InitValueRuntimeException("setMode", initsHashMap.get("setMode"));
+                }
+            } else {
+                throw new RuntimeException("Unimplemented hardware element: " +
+                        hardwareType.getName());
+            }
+        }
+    }
+
+    private static void InitValueRuntimeException(String key, String value) {
+        throw new RuntimeException("Unimplemented hardware element method '" +
+                key + ":" + value + "' encountered.");
     }
 }
-
-/* eg. initialization code
- *
- * public Hardware robot = new Hardware(telemetry, hardwareMap);
- * robot.introduce(new HardwareElement("leftFront", "leftFront", DcMotorEx.class));
- * robot.introduce(new HardwareElement("rightFront", "rightFront", DcMotorEx.class,
- *          "setDirection:REVERSE"));
- *
- * robot.hardwareElements.leftFront.setPower(0.5);
- */
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HardwareElement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HardwareElement.java
@@ -1,45 +1,62 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import java.util.HashMap;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 
+/** Creates Objects to store hardware elements and initializers.
+ * HardwareElement's are essentially HardwareDevices plus initializers. */
 public class HardwareElement {
-    /** The type of the device. */
-    private final java.lang.Class type;
-
-    /** The key that can be accessed from the Hardware class. */
-    private final String key;
-    /** The value that is expected to be in the hardware map. */
-    private final String entry;
-     /** Key: The initializing feature being referred to.
-     * Value: The value the initializing feature will be set to. */
-    private HashMap<String, String> initializers = new HashMap<String, String>();
-
-    /** Hashmap with default pairs of initializingFeature:defaultValue.*/
-    public static HashMap<String, String> DEFAULT_INITIALIZERS = new HashMap<String,String>() {{
-            DEFAULT_INITIALIZERS.put("setDirection", "FORWARD");
-            DEFAULT_INITIALIZERS.put("setZeroPowerBehavior", "BRAKE");
-            DEFAULT_INITIALIZERS.put("setMode", "RUN_USING_ENCODER");
-        }};
+    /** Stores value that is expected to be in the hardware map.
+     * Expected to be the same as the object name in init conditionals. */
+    public String entry;
+    public java.lang.Class<HardwareDevice> type;
+    public HardwareDevice device;
+    public HashMap<String, String> initializers = new HashMap<>();
 
     /** Constructs a HardwareElement.
-     * @param type The type of the device.
-     * @param key The key that can be accessed from the Hardware class.
-     * @param entry The value that is expected to be in the hardware map. */
-    public HardwareElement(java.lang.Class type, String key, String entry) {
-        this.type = type;
-        this.key = key;
+     * @param deviceType The type of the device.
+     * @param entry The value that is expected to be in the hardware map.
+     *      Assumes Key and Entry are the same.
+     * @param hardwareMap The FTC SDK hardware map.
+     * */
+    public HardwareElement(HardwareMap hardwareMap, java.lang.Class<HardwareDevice> deviceType, String entry) {
         this.entry = entry;
-        this.initializers = DEFAULT_INITIALIZERS;
+        this.device = hardwareMap.get(type, entry);
+        this.type = deviceType;
+        setDefaultInitializers(deviceType);
     }
 
-    /** Format initializers: key:value,key:value
-     * eg. "setDirection:REVERSE,setZeroPowerBehavior:BRAKE"
-     * ONLY NECESSARY TO LIST NON-DEFAULT PAIRS. */
-    public HardwareElement(java.lang.Class type, String key, String entry, String initializers) {
-        this.type = type;
-        this.key = key;
+    /** /** Constructs a HardwareElement.
+     * @param deviceType The type of the device.
+     * @param entry The value that is expected to be in the hardware map.
+     *      Assumes Key and Entry are the same.
+     * @param hardwareMap The FTC SDK hardware map.
+     * @param initializers: key:value,key:value
+     *      eg. "setDirection:REVERSE,setZeroPowerBehavior:BRAKE"
+     *      Only necessary to list non-default pairs. */
+    public HardwareElement(HardwareMap hardwareMap, java.lang.Class<HardwareDevice> deviceType, String entry, String initializers) {
         this.entry = entry;
+        this.device = hardwareMap.get(deviceType, entry);
+        this.type = deviceType;
         this.initializers = createHashMap(initializers);
+    }
+
+    /** Sets default initializers given device type.
+     * Update this method when implementing new initializers. **/
+    private void setDefaultInitializers(java.lang.Class<HardwareDevice> deviceType) {
+        if (deviceType.getName().equals("DcMotor")) {
+            initializers.put("setDirection", "FORWARD");
+            initializers.put("setZeroPowerBehavior", "BRAKE");
+            initializers.put("setMode", "RUN_USING_ENCODER");
+        } else if (deviceType.getName().equals("DcMotorEx")) {
+            initializers.put("setDirection", "FORWARD");
+            initializers.put("setZeroPowerBehavior", "BRAKE");
+            initializers.put("setMode", "RUN_USING_ENCODER");
+        } else {
+            throw new RuntimeException(deviceType.getName() + " has not been " +
+                    "implemented with default initializers.");
+        }
     }
 
     /** Takes in string formatted "key:value,key:value" and creates HashMap. */
@@ -55,11 +72,10 @@ public class HardwareElement {
         HashMap<String, String> initializerHashMap = new HashMap<String,String>();
 
         /* Iterates through default initializers, checking if the HashMap created by inputs already
-         * includes the key. If so, it adds that key:value pair to initializerHashMap.
-         * If not, it adds the default key:value pair. */
-        for (HashMap.Entry<String, String> defaultInitPair : DEFAULT_INITIALIZERS.entrySet()) {
+         * includes the key. If so, it changes the value to the input-provided value. */
+        for (HashMap.Entry<String, String> defaultInitPair : initializers.entrySet()) {
             if(inputHashMap.containsKey(defaultInitPair.getKey())) {
-                initializerHashMap.put(defaultInitPair.getKey(),
+                initializers.put(defaultInitPair.getKey(),
                         initializerHashMap.get(defaultInitPair.getKey()));
             } else {
                 initializerHashMap.put(defaultInitPair.getKey(), defaultInitPair.getValue());
@@ -68,7 +84,7 @@ public class HardwareElement {
 
         /** Checks if any input initialization features are given that are not yet listed. */
         for (HashMap.Entry<String, String> inputInitPair : inputHashMap.entrySet()) {
-            if(!DEFAULT_INITIALIZERS.containsKey(inputInitPair.getKey())){
+            if(!initializers.containsKey(inputInitPair.getKey())){
                 throw new RuntimeException(inputInitPair.getKey() +
                         " is not currently listed as an initialization feature.");
             }
