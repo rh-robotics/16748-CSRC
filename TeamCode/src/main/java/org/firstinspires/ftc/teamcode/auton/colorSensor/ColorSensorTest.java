1147cc26baa9ec79e8a44fcfd81a966ba45d3eb5
sadie-datoo <sadiedatoo@rowlandhall.org>
2023-10-25 12:57:29 -0600

    Fixed variable and class name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/colorSensor/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/colorSensor/ColorSensorTest.java
@@ -3,17 +3,17 @@ package org.firstinspires.ftc.teamcode.auton.colorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-public class colorSensorTest extends LinearOpMode {
-    ColorSensor color_sensor;
+public class ColorSensorTest extends LinearOpMode {
+    ColorSensor colorSensor;
 
     public void runOpMode() {
-        color_sensor = hardwareMap.colorSensor.get("color");
+        colorSensor = hardwareMap.colorSensor.get("color");
 
         waitForStart();
 
         while (opModeIsActive()) {
             //finds amount of red, green, blue, and light (alpha)
-            telemetry.addData("hue: ", color_sensor.argb());
+            telemetry.addData("hue: ", colorSensor.argb());
             telemetry.update();
         }
     }
