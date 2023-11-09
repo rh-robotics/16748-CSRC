bb98b6ba8c1cd3d636718394455750fcc7fca34b
sadie-datoo <sadiedatoo@rowlandhall.org>
2023-11-08 17:14:03 -0700

    Basic Touch Sensor Proof of Concept

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TouchSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TouchSensorTest.java
@@ -1,26 +1,18 @@
 package org.firstinspires.ftc.teamcode.auton;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-import org.firstinspires.ftc.teamcode.subsystems.hardware.Hardware;
-import org.firstinspires.ftc.teamcode.subsystems.hardware.HardwareElement;
-
-@Autonomous(name = "Touch Sensor Test", group = "Testing Touch Sensor")
+@Autonomous(name="Touch Sensor Test")
 public class TouchSensorTest extends OpMode {
-    Hardware robot;
-
+    TouchSensor touch;
     @Override
-    public void init() {
-        telemetry.addLine("Initializing robot");
-        robot = new Hardware(hardwareMap, telemetry);
-        robot.introduce(new HardwareElement<>(TouchSensor.class, hardwareMap, "touchSensor"));
-        telemetry.addLine("Initialized robot");
-    }
+    public void init() {}
 
     @Override
     public void loop() {
-        telemetry.addData("Touch sensor", robot.<TouchSensor>get("touchSensor").isPressed() ? "pressed" : "not-pressed");
+        touch = hardwareMap.touchSensor.get("touchSensor");
+        telemetry.addData("Touch Sensor", touch.isPressed());
     }
 }
