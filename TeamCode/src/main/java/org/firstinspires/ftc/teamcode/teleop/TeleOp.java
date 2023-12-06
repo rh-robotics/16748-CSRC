4daf98e68be076b05aad819e5925246b5beee7d9
Picklze1 <georgejensen@rowlandhall.org>
2023-12-06 00:22:23 -0700

    Fixes WOO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
@@ -53,15 +53,42 @@ public class TeleOp extends OpMode {
         robot.introduce(new HardwareElement<>(DcMotor.class, hardwareMap, "rightViperSlideMotor", "setMode(DcMotor.RunMode.RUN_USING_ENCODER)"));
 
         telemetry.addData("Status", "Initialized");
+        telemetry.addData("Controls:", "\n" +
+                "        Left Stick y = Drive\n" +
+                "        Left Stick x = Strafe\n" +
+                "        Right Stick x = Turn\n" +
+                "        Right Bumper = Joint +\n" +
+                "        Right Trigger = Joint -\n" +
+                "        Left Bumper = Automatic Scoring\n" +
+                "        Left Trigger = VS, Arm and Claw reset (Auto)\n" +
+                "        A = Activate Intake (Hold)\n" +
+                "        X = Claw + (Manual)\n" +
+                "        Y = Claw - (Manual)\n" +
+                "        D Pad Up = VS Up (Manual)\n" +
+                "        D Pad Down = VS Down (Manual)");
     }
 
     @Override
     public void loop() {
+        /* Controls:
+         * Left Stick y = Drive
+         * Left Stick x = Strafe
+         * Right Stick x = Turn
+         * Right Bumper = Joint +
+         * Right Trigger = Joint -
+         * Left Bumper = Automatic Scoring
+         * Left Trigger = VS, Arm and Claw reset (Auto)
+         * A = Activate Intake (Hold)
+         * X = Claw + (Manual)
+         * Y = Claw - (Manual)
+         * D Pad Up = VS Up (Manual)
+         * D Pad Down = VS Down (Manual)
+         */
 
         // Values for drive.
         drive = gamepad1.left_stick_y * 0.8;
-        turn = -gamepad1.left_stick_x * 0.6;
-        strafe = gamepad1.right_stick_x * 0.8;
+        turn = -gamepad1.right_stick_x * 0.6;
+        strafe = gamepad1.left_stick_x * 0.8;
 
         // Set power to values calculated above.
         robot.<DcMotor>get("leftFront").setPower(leftFPower);
@@ -82,14 +109,14 @@ public class TeleOp extends OpMode {
             telemetry.addData("Scoring Pos", "Reset");
         }
 
-        // Move claw via x, and y.
+        // Move claw via x and y.
         if (gamepad1.x) {
             robot.<Servo>get("clawServo").setPosition(robot.<Servo>get("clawServo").getPosition() + -0.125);
         } else if (gamepad1.y) {
             robot.<Servo>get("clawServo").setPosition(robot.<Servo>get("clawServo").getPosition() + 0.125);
         }
 
-        // Moving VS maually via dpad up, and down.
+        // Moving VS maually via dpad up and down.
         if (gamepad1.dpad_up) {
             robot.<DcMotor>get("leftViperSlideMotor").setPower(0.5);
             robot.<DcMotor>get("rightViperSlideMotor").setPower(0.5);
@@ -122,7 +149,7 @@ public class TeleOp extends OpMode {
         }
     }
 
-    // Scoring method, all together like a bowl of chilli.
+    // Scoring method, all together like a bowl of chili.
     public void scoring() {
         telemetry.addData("Scoring Status", "Started");
 
