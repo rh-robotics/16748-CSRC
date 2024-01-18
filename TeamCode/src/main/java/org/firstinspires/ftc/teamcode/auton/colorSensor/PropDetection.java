33927072ca96d4e879c9b0beef8bee7b0bbd19d8
Picklze1 <georgejensen@rowlandhall.org>
2024-01-18 01:51:26 -0700

    Added Prop detection with Color Sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/colorSensor/PropDetection.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.auton.colorSensor;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.teamcode.subsystems.hardware.Hardware;
+import org.firstinspires.ftc.teamcode.subsystems.hardware.HardwareElement;
+
+@Autonomous(name = "Prop Dectection")
+public class PropDetection extends OpMode {
+
+    private Hardware robot;
+    int red;
+    int colorTolerence = 30;
+    boolean propDetected = false;
+
+    @Override
+    public void init() {
+        robot = new Hardware(hardwareMap, telemetry);
+
+        robot.introduce(new HardwareElement<>(ColorSensor.class, hardwareMap, "colorSensor"));
+
+        telemetry.addData("Status", "Init");
+    }
+
+    @Override
+    public void loop() {
+
+        red = robot.<ColorSensor>get("colorSensor").red();
+
+        if (red - colorTolerence < red && red < red + colorTolerence) {
+            propDetected = true;
+        }
+    }
+}
